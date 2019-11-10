46917d35b7bb39df28805faa24e3326269e2ddc4
liyang <1032199882@qq.com>
2019-11-10 14:04:38 +0800

    Signed-off-by: liyang <1032199882@qq.com>

--- src/pages/todo/todo.js
+++ src/pages/todo/todo.js
@@ -1,5 +1,6 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
+import { AtTabs, AtTabsPane } from 'taro-ui'
 import './todo.scss'
 import TodoList from '../../component/todo/todo-list'
 import {guid} from '../../utils/tool.js'
@@ -10,6 +11,8 @@ const LIST = [
   { id: guid(), checked: true, text: '看电视' },
 ]
 
+const TABLIST = [{ title: '全部' }, { title: '完成' }]
+
 export default class Todo extends Component {
 
   config = {
@@ -19,7 +22,8 @@ export default class Todo extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      list: LIST
+      list: LIST,
+      tabActive: 0
     }
   }
 
@@ -33,6 +37,12 @@ export default class Todo extends Component {
 
   componentDidHide () { }
 
+  handleTabClick(value) {
+    this.setState({
+      tabActive: value
+    })
+  }
+
   // TodoList onchange
   tLOnChange(value) {
     this.setState({
@@ -44,7 +54,14 @@ export default class Todo extends Component {
 
     return (
       <View className='todo'>
-        <TodoList list={this.state.list} onChange={this.tLOnChange.bind(this)} />
+        <AtTabs swipeable={false} current={this.state.tabActive} tabList={TABLIST} onClick={this.handleTabClick.bind(this)}>
+          <AtTabsPane className='tab_0' current={this.state.tabActive} index={0} >
+            <TodoList list={this.state.list} onChange={this.tLOnChange.bind(this)} />
+          </AtTabsPane>
+          <AtTabsPane current={this.state.tabActive} index={1}>
+            <View style='padding: 100px 50px;background-color: #FAFBFC;text-align: center;'>标签页二的内容</View>
+          </AtTabsPane>
+        </AtTabs>
       </View>
     )
   }
